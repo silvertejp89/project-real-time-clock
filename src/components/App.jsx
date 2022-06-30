635e07766a3d949490ca67372e8c671e2fea42bb
silvertejp89 <kriss.stockhaus@gmail.com>
2022-06-30 11:12:59 +0200

    use useState hook to get and update the time with onClick on start-clock-button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // const now = new Date().toLocaleTimeString('en-GB');
+
+  const [time, setTime] = useState('00:00:00');
+
+  function upDateTime() {
+    const newTime = new Date().toLocaleTimeString('en-GB');
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <button>Get Time</button>
-      <h1>12:15</h1>
+      <button onClick={upDateTime}>Get Time</button>
+      <h1> {time} </h1>
       <button>Start Clock</button>
     </div>
   );
 }
 
 export default App;
+
+//button own component?
 
